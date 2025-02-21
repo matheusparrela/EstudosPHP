775915526b7ec55360522e81c6f0a4c312e9c359
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2025-02-20 22:25:05 -0300

    Meu Primeiro PHP
    Minha primeira página PHP.

---
+++ ex000/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me Livrando da Maldição</title>
+</head>
+
+<body>
+    <h1>"Olá, Mundo!</h1>
+        <?php 
+
+            $nome = "João";
+            $sobrenome = "da Silva";
+            const PAIS = "Brasil";
+            echo "Muito prazer, $nome $sobrenome! Você mora no ".PAIS."!";
+        ?>
+    
+    <p>Meu primeiro paragráfo!</p>
+</body>
+
+</html>
