3e351174cb2bada9000ff1196befe3bc12a055bd
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2025-02-20 22:28:46 -0300

    Variáveis em PHP
    Testando variáveis em PHP.

---
+++ ex003/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+
+<body>
+    <h1>Teste dos Tipos Primitivos</h1>
+
+    <?php
+
+    // $num = 3e1;
+    // echo "O valor da variavel é $num";
+    
+    // $v = 3e1;
+    // var_dump($v);
+
+
+    // $var = "Feliz Aniversário!";
+    // print "$var";
+
+    // Forçando o tipo de variável - Coerção
+    // $var = "150";
+    // $num = (int) $var;
+    // var_dump($num);
+
+    // // Vetor
+    // $vet = [1, 2, 3, 4];
+    // var_dump($vet);
+    
+    // //Diferação entre Aspas Duplas e Simples
+    // $nome = 'João';
+    // $sobrenome = 'da Silva;
+    // echo "$nome $sobrenome \u{1F596}";
+
+    // //Interpolação com PHP 
+    // const ESTADO = "MG";
+    // echo "<br>Moro em ESTADO"; //=> "Moro em Estado"
+    // echo '<br>Moro em ESTADO'; //=> "Moro em Estado"
+    // echo "<br>Moro em ".ESTADO; //=> "Moro no AC"
+    
+    //String Em Heredoc
+    // $curso = "PHP";
+    // $ano = date('Y');
+    // echo <<< FRASE 
+    //     Estou Estudando $curso em $ano"
+    //     FRASE;
+
+
+    ?>
+
+</body>
+
+</html>
